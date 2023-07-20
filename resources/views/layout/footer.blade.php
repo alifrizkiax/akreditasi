c36fd5f147fec84cfff0266e4a6fb508538fbd2b
alifrizkiax <alifrizkiax@gmail.com>
2023-07-20 23:12:13 +0700

    beni ngerjain ni

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer-left">
-    Copyright &copy; 2023 <div class="bullet"></div> Created By <a href="">Refina Aninda Legia</a>
+    Copyright &copy; 2023 <div class="bullet"></div> Created By <a href="">Refina Aninda Legia, Alif Rizki Ananta H</a>
   </div>
   <div class="footer-right">
 
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,10 +1,10 @@
 <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      D3 Teknik Informatika
+      D3 Keperawatan
     </div>
     <ul class="sidebar-menu">
       <li class="menu-header">Dashboard</li>
-      <li class="dropdown">
+      <li class="dropdown"> 
         <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
         <ul class="dropdown-menu">
           <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
@@ -13,8 +13,26 @@
       </li>
     </ul>
 
-    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+    <div class="mt-1 mb-1 p-1 hide-sidebar-mini">
       <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
         <i class="fas fa-rocket"></i> Documentation
       </a>
-    </div>        </aside>
+    </div>       
+  
+    <div class="mt-1 mb-1 p-1 hide-sidebar-mini">
+      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+        <i class="fas fa-rocket"></i> Documentation
+      </a>
+    </div> 
+  
+    <div class="mt-1 mb-1 p-1 hide-sidebar-mini">
+      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+        <i class="fas fa-rocket"></i> Documentation
+      </a>
+    </div> 
+  
+    <div class="mt-1 mb-1 p-1 hide-sidebar-mini">
+      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+        <i class="fas fa-rocket"></i> Documentation
+      </a>
+    </div> </aside>
