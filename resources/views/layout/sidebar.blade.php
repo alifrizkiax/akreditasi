2a7170660d8b475a448bd5f48eb0dccceb5f5f12
alifrizkiax <alifrizkiax@gmail.com>
2023-07-22 20:44:21 +0700

    rada login sedikit

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,11 +1,11 @@
 <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      D3 Keperawatan
+     Aplikasi Akreditasi Polindra
     </div>
     <ul class="sidebar-menu">
       <li class="menu-header">Dashboard</li>
       <li class="dropdown"> 
-        <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+        <a href="dashboard" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
         <ul class="dropdown-menu">
           <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
           <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
@@ -14,25 +14,38 @@
     </ul>
 
     <div class="mt-1 mb-1 p-1 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Documentation
-      </a>
+    <ul class="sidebar-menu">
+      <li class="dropdown"> 
+        <a href="," class=><i class="fas fa-fire"></i><span>Users</span></a>
+        <ul class="dropdown-menu">
+        </ul>
+      </li>
+    </ul> 
     </div>       
+
+    <div class="mt-1 mb-1 p-1 hide-sidebar-mini ">
+    <ul class="sidebar-menu">
+      <li class="dropdown"> 
+        <a href="dashboard" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Data Jurusan</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="index-0.html">Prodi D3 Keperawatan</a></li>
+          <li><a class="nav-link" href="index.html">Prodi D3 TI</a></li>
+        </ul>
+      </li>
+    </ul>
+    </div>   
   
-    <div class="mt-1 mb-1 p-1 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Documentation
-      </a>
+    <div class="mt-1 mb-1 p-1 hide-sidebar-mini ">
+    <ul class="sidebar-menu">
+      <li class="dropdown"> 
+        <a href="dashboard" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Master Instrumen</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="index-0.html">Standar</a></li>
+          <li><a class="nav-link" href="index.html">Kode</a></li>
+          <li><a class="nav-link" href="index-0.html">SubKode</a></li>
+          <li><a class="nav-link" href="index.html">Deskripsi Dokumen</a></li>
+        </ul>
+      </li>
+    </ul>
     </div> 
-  
-    <div class="mt-1 mb-1 p-1 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Documentation
-      </a>
-    </div> 
-  
-    <div class="mt-1 mb-1 p-1 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Documentation
-      </a>
-    </div> </aside>
+  </aside>
